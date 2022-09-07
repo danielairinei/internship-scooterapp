4a5aa8a5dc26abe49e6e7ff33ca794ca6df37b9d
Daniel <airinei.daniel27@gmail.com>
2022-09-07 18:03:44 +0300

    feature : api-calls

--- app/src/main/java/com/internship/move/data/dto/user/UserDto.kt
+++ app/src/main/java/com/internship/move/data/dto/user/UserDto.kt
@@ -1,13 +1,17 @@
 package com.internship.move.data.dto.user
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class UserDto(
-    val email: String,
-    val name: String,
-    val password: String,
-    val id: String,
-    val drivingLicense: String,
-    val validated: Boolean
+    @Json(name = "__v") val __v: Int,
+    @Json(name = "_id") val _id: String,
+    @Json(name = "createdAt") val createdAt: String,
+    @Json(name = "drivingLicense") val drivinglicense: String,
+    @Json(name = "email") val email: String,
+    @Json(name = "name") val name: String,
+    @Json(name = "password") val password: String,
+    @Json(name = "updatedAt") val updatedAt: String,
+    @Json(name = "validated") val validated: Boolean
 )
