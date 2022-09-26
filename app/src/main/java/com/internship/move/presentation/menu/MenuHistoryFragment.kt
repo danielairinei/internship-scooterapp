1d89fcd91f43365138b54f44b3082d1bfabb2e92
Daniel <airinei.daniel27@gmail.com>
2022-09-26 17:16:47 +0300

    features : -menu ui completed -menu api calls completed
    TODO :
    -ui for scooter ride, ride review etc

--- app/src/main/java/com/internship/move/presentation/menu/MenuHistoryFragment.kt
+++ app/src/main/java/com/internship/move/presentation/menu/MenuHistoryFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.internship.move.R
 import com.internship.move.databinding.FragmentMenuHistoryBinding
+import com.internship.move.presentation.menu.adapter.RideHistoryAdapter
+import com.internship.move.presentation.menu.adapter.RideHistoryItem
+import com.internship.move.presentation.menu.adapter.RideHistoryItemDecorator
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 
 class MenuHistoryFragment : Fragment(R.layout.fragment_menu_history) {
 
     private val binding by viewBinding(FragmentMenuHistoryBinding::bind)
+    private val recyclerAdapter by lazy { RideHistoryAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -18,5 +23,15 @@ class MenuHistoryFragment : Fragment(R.layout.fragment_menu_history) {
         binding.toolbar.setNavigationOnClickListener {
             findNavController().navigate(MenuHistoryFragmentDirections.actionMenuHistoryFragmentToMenuFragment())
         }
+
+        binding.historyRV.adapter = recyclerAdapter
+        binding.historyRV.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        binding.historyRV.addItemDecoration(RideHistoryItemDecorator(resources.getDimension(R.dimen.history_card_view_spacing)))
+
+
+        recyclerAdapter.submitList(getRides())
+
     }
+
+    private fun getRides(): List<RideHistoryItem> = (1..10).map { index -> RideHistoryItem(index) }
 }
---
+++ app/src/main/java/com/internship/move/presentation/menu/adapter/RideHistoryViewHolder.kt
@@ -0,0 +1,18 @@
+package com.internship.move.presentation.menu.adapter
+
+import android.annotation.SuppressLint
+import androidx.recyclerview.widget.RecyclerView
+import com.internship.move.databinding.ItemHistoryCardViewBinding
+
+class RideHistoryViewHolder(private val binding: ItemHistoryCardViewBinding) : RecyclerView.ViewHolder(binding.root){
+
+    @SuppressLint("SetTextI18n")
+    fun bind(rideHistoryItem: RideHistoryItem) {
+        binding.startLocationTV.text = rideHistoryItem.startAddress
+        binding.endLocationTV.text = rideHistoryItem.endAddress
+        binding.travelTimeTV.text = rideHistoryItem.time
+        binding.distanceTV.text = "${rideHistoryItem.distance} km"
+    }
+
+
+}
